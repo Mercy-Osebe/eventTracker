e638eeae88486f779b037ccac1ea6bfc2ded9279
Mercy Osebe <marcynyaata@gmail.com>
2022-05-20 23:09:21 +0300

    adding event api controller

---
+++ controllers/api/EventsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers\api;
+
+use Yii;
+use yii\rest\Controller;
+
+class EventsController extends Controller
+{
+
+    public function actionReport(){
+        return "TODO";
+    }
+
+}
