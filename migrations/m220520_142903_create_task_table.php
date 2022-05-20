ba712bfc2733daa9196563c26089c45f42fb4ac4
Mercy Osebe <marcynyaata@gmail.com>
2022-05-20 17:40:47 +0300

    added tasks migration

---
+++ migrations/m220520_142903_create_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%task}}`.
+ */
+class m220520_142903_create_task_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%task}}', [
+            'id' => $this->primaryKey(),
+            'program_time' => $this->string(50)->notNull(),
+            'event' => $this->string(10)->notNull(),
+            'message' => $this->text()->notNull(),
+            'actual_time' => $this->string(50)->notNull(),
+            'display_message' => $this->text()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%task}}');
+    }
+}
