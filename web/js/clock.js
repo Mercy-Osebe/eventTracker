533018af991b48d0422d3a6d390ddd1d44b25e21
Mercy Osebe <marcynyaata@gmail.com>
2022-05-20 23:01:43 +0300

    making ui clock start at 12

--- web/js/clock.js
+++ web/js/clock.js
@@ -1,8 +1,16 @@
+
+ d = new Date();
+ d.setHours(12);
+ d.setMinutes(0);
+ d.setSeconds(0);
+
 setInterval(() => {
-    d = new Date(); //object of date()
+   
+    d.setSeconds(d.getSeconds() + 1);
     hr = d.getHours();
     min = d.getMinutes();
     sec = d.getSeconds();
+
     hr_rotation = 30 * hr + min / 2; //converting current time
     min_rotation = 6 * min;
     sec_rotation = 6 * sec;
@@ -11,3 +19,4 @@ setInterval(() => {
     minute.style.transform = `rotate(${min_rotation}deg)`;
     second.style.transform = `rotate(${sec_rotation}deg)`;
 }, 1000);
+
