93b568be4a628c9fc13631b3be5fa95f59e6c9ca
Mercy Osebe <marcynyaata@gmail.com>
2022-05-22 19:41:39 +0300

    Giving application a cool name

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'name' => 'Event Tracker',
     'timeZone' => 'Africa/Nairobi',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
